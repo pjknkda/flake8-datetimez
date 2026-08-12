e200aab814bb882a43627a56923541910c3c03a7
Jungkook Park <pjknkda@gmail.com>
2026-08-12 10:12:31 +0000

    style: format the code with ruff
    Add a ruff config (line length 120, E/F/W/I) and reformat everything with
    `ruff format`. E402 is ignored because `__version__` has to stay above the
    imports for setup.py to read it.
    
    `ruff format` normalises the quotes around `__version__`, so the version
    regex in setup.py now accepts either quote style.

--- flake8_datetimez.py
+++ flake8_datetimez.py
@@ -1,4 +1,4 @@
-__version__ = '20.10.0'
+__version__ = "20.10.0"
 
 import ast
 from collections import namedtuple
@@ -19,7 +19,7 @@ def _get_from_keywords(keywords, arg):
 
 
 class DateTimeZChecker:
-    name = 'flake8.datetimez'
+    name = "flake8.datetimez"
     version = __version__
 
     def __init__(self, tree, filename):
@@ -27,14 +27,14 @@ class DateTimeZChecker:
         self.filename = filename
 
     def run(self):
-        if self.filename in ('stdin', '-', None):
-            self.filename = 'stdin'
+        if self.filename in ("stdin", "-", None):
+            self.filename = "stdin"
             self.lines = pycodestyle.stdin_get_value().splitlines(True)
         else:
             self.lines = pycodestyle.readlines(self.filename)
 
         if not self.tree:
-            self.tree = ast.parse(''.join(self.lines))
+            self.tree = ast.parse("".join(self.lines))
 
         for node in ast.walk(self.tree):
             for child_node in ast.iter_child_nodes(node):
@@ -48,102 +48,105 @@ class DateTimeZChecker:
 
 
 class DateTimeZVisitor(ast.NodeVisitor):
-
     def __init__(self):
         self.errors = []
 
     def visit_Call(self, node):
         # ex: `datetime.something()``
-        is_datetime_class = (isinstance(node.func, ast.Attribute)
-                             and isinstance(node.func.value, ast.Name)
-                             and node.func.value.id == 'datetime')
+        is_datetime_class = (
+            isinstance(node.func, ast.Attribute)
+            and isinstance(node.func.value, ast.Name)
+            and node.func.value.id == "datetime"
+        )
 
         # ex: `datetime(2000, 1, 1, 0, 0, 0, 0)`
-        is_unqualified_datetime_class_call = isinstance(node.func, ast.Name) and node.func.id == 'datetime'
+        is_unqualified_datetime_class_call = isinstance(node.func, ast.Name) and node.func.id == "datetime"
 
         # ex: `datetime.datetime.something()``
-        is_datetime_module_n_class = (isinstance(node.func, ast.Attribute)
-                                      and isinstance(node.func.value, ast.Attribute)
-                                      and node.func.value.attr == 'datetime'
-                                      and isinstance(node.func.value.value, ast.Name)
-                                      and node.func.value.value.id == 'datetime')
+        is_datetime_module_n_class = (
+            isinstance(node.func, ast.Attribute)
+            and isinstance(node.func.value, ast.Attribute)
+            and node.func.value.attr == "datetime"
+            and isinstance(node.func.value.value, ast.Name)
+            and node.func.value.value.id == "datetime"
+        )
 
-        if (is_datetime_class and node.func.attr == 'datetime') or is_unqualified_datetime_class_call:
+        if (is_datetime_class and node.func.attr == "datetime") or is_unqualified_datetime_class_call:
             # ex `datetime(2000, 1, 1, 0, 0, 0, 0, datetime.timezone.utc)`
-            is_case_1 = (len(node.args) == 8
-                            and not (isinstance(node.args[7], ast.Constant)
-                                    and node.args[7].value is None))
+            is_case_1 = len(node.args) == 8 and not (
+                isinstance(node.args[7], ast.Constant) and node.args[7].value is None
+            )
 
             # ex `datetime.datetime(2000, 1, 1, tzinfo=datetime.timezone.utc)`
-            tzinfo_keyword = _get_from_keywords(node.keywords, 'tzinfo')
-            is_case_2 = (tzinfo_keyword is not None
-                            and not (isinstance(tzinfo_keyword.value, ast.Constant)
-                                    and tzinfo_keyword.value.value is None))
+            tzinfo_keyword = _get_from_keywords(node.keywords, "tzinfo")
+            is_case_2 = tzinfo_keyword is not None and not (
+                isinstance(tzinfo_keyword.value, ast.Constant) and tzinfo_keyword.value.value is None
+            )
 
             if not (is_case_1 or is_case_2):
                 self.errors.append(DTZ001(node.lineno, node.col_offset))
 
         if is_datetime_class or is_datetime_module_n_class:
-            if node.func.attr == 'today':
+            if node.func.attr == "today":
                 self.errors.append(DTZ002(node.lineno, node.col_offset))
 
-            elif node.func.attr == 'utcnow':
+            elif node.func.attr == "utcnow":
                 self.errors.append(DTZ003(node.lineno, node.col_offset))
 
-            elif node.func.attr == 'utcfromtimestamp':
+            elif node.func.attr == "utcfromtimestamp":
                 self.errors.append(DTZ004(node.lineno, node.col_offset))
 
-            elif node.func.attr in 'now':
+            elif node.func.attr in "now":
                 # ex: `datetime.now(UTC)`
-                is_case_1 = (len(node.args) == 1
-                             and len(node.keywords) == 0
-                             and not (isinstance(node.args[0], ast.Constant)
-                                      and node.args[0].value is None))
+                is_case_1 = (
+                    len(node.args) == 1
+                    and len(node.keywords) == 0
+                    and not (isinstance(node.args[0], ast.Constant) and node.args[0].value is None)
+                )
 
                 # ex: `datetime.now(tz=UTC)`
-                tz_keyword = _get_from_keywords(node.keywords, 'tz')
-                is_case_2 = (tz_keyword is not None
-                             and not (isinstance(tz_keyword.value, ast.Constant)
-                                      and tz_keyword.value.value is None))
+                tz_keyword = _get_from_keywords(node.keywords, "tz")
+                is_case_2 = tz_keyword is not None and not (
+                    isinstance(tz_keyword.value, ast.Constant) and tz_keyword.value.value is None
+                )
 
                 if not (is_case_1 or is_case_2):
                     self.errors.append(DTZ005(node.lineno, node.col_offset))
 
-            elif node.func.attr == 'fromtimestamp':
+            elif node.func.attr == "fromtimestamp":
                 # ex: `datetime.fromtimestamp(1234, UTC)`
-                is_case_1 = (len(node.args) == 2
-                             and len(node.keywords) == 0
-                             and not (isinstance(node.args[1], ast.Constant)
-                                      and node.args[1].value is None))
+                is_case_1 = (
+                    len(node.args) == 2
+                    and len(node.keywords) == 0
+                    and not (isinstance(node.args[1], ast.Constant) and node.args[1].value is None)
+                )
 
                 # ex: `datetime.fromtimestamp(1234, tz=UTC)`
-                tz_keyword = _get_from_keywords(node.keywords, 'tz')
-                is_case_2 = (tz_keyword is not None
-                             and not (isinstance(tz_keyword.value, ast.Constant)
-                                      and tz_keyword.value.value is None))
+                tz_keyword = _get_from_keywords(node.keywords, "tz")
+                is_case_2 = tz_keyword is not None and not (
+                    isinstance(tz_keyword.value, ast.Constant) and tz_keyword.value.value is None
+                )
 
                 if not (is_case_1 or is_case_2):
                     self.errors.append(DTZ006(node.lineno, node.col_offset))
 
-            elif node.func.attr == 'strptime':
-                parent = getattr(node, '_flake8_datetimez_parent', None)
-                pparent = getattr(parent, '_flake8_datetimez_parent', None)
+            elif node.func.attr == "strptime":
+                parent = getattr(node, "_flake8_datetimez_parent", None)
+                pparent = getattr(parent, "_flake8_datetimez_parent", None)
 
                 # ex: `datetime.strptime(...).replace(tzinfo=UTC)`
-                if not (isinstance(parent, ast.Attribute)
-                        and parent.attr == 'replace'):
+                if not (isinstance(parent, ast.Attribute) and parent.attr == "replace"):
                     is_case_1 = False
                 elif not isinstance(pparent, ast.Call):
                     is_case_1 = False
                 else:
-                    tzinfo_keyword = _get_from_keywords(pparent.keywords, 'tzinfo')
-                    is_case_1 = (tzinfo_keyword is not None
-                                 and not (isinstance(tzinfo_keyword.value, ast.Constant)
-                                          and tzinfo_keyword.value.value is None))
+                    tzinfo_keyword = _get_from_keywords(pparent.keywords, "tzinfo")
+                    is_case_1 = tzinfo_keyword is not None and not (
+                        isinstance(tzinfo_keyword.value, ast.Constant) and tzinfo_keyword.value.value is None
+                    )
 
                 # ex: `datetime.strptime(...).astimezone()`
-                if not (isinstance(parent, ast.Attribute)
-                        and parent.attr == 'astimezone'):
+                if not (isinstance(parent, ast.Attribute) and parent.attr == "astimezone"):
                     is_case_2 = False
                 elif not isinstance(pparent, ast.Call):
                     is_case_2 = False
@@ -151,75 +154,68 @@ class DateTimeZVisitor(ast.NodeVisitor):
                     is_case_2 = True
 
                 # ex: `datetime.strptime(..., '...%z...')`
-                is_case_3 = (1 < len(node.args)
-                             and isinstance(node.args[1], STRING_NODE)
-                             and '%z' in node.args[1].s)
+                is_case_3 = 1 < len(node.args) and isinstance(node.args[1], STRING_NODE) and "%z" in node.args[1].s
 
                 if not (is_case_1 or is_case_2 or is_case_3):
                     self.errors.append(DTZ007(node.lineno, node.col_offset))
 
         # ex: `date.something()``
-        is_date_class = (isinstance(node.func, ast.Attribute)
-                         and isinstance(node.func.value, ast.Name)
-                         and node.func.value.id == 'date')
+        is_date_class = (
+            isinstance(node.func, ast.Attribute)
+            and isinstance(node.func.value, ast.Name)
+            and node.func.value.id == "date"
+        )
 
         # ex: `datetime.date.something()``
-        is_date_module_n_class = (isinstance(node.func, ast.Attribute)
-                                  and isinstance(node.func.value, ast.Attribute)
-                                  and node.func.value.attr == 'date'
-                                  and isinstance(node.func.value.value, ast.Name)
-                                  and node.func.value.value.id == 'datetime')
+        is_date_module_n_class = (
+            isinstance(node.func, ast.Attribute)
+            and isinstance(node.func.value, ast.Attribute)
+            and node.func.value.attr == "date"
+            and isinstance(node.func.value.value, ast.Name)
+            and node.func.value.value.id == "datetime"
+        )
 
         if is_date_class or is_date_module_n_class:
-            if node.func.attr == 'today':
+            if node.func.attr == "today":
                 self.errors.append(DTZ011(node.lineno, node.col_offset))
 
-            elif node.func.attr == 'fromtimestamp':
+            elif node.func.attr == "fromtimestamp":
                 self.errors.append(DTZ012(node.lineno, node.col_offset))
 
         self.generic_visit(node)
 
 
-error = namedtuple('error', ['lineno', 'col', 'message', 'type'])
+error = namedtuple("error", ["lineno", "col", "message", "type"])
 Error = partial(partial, error, type=DateTimeZChecker)
 
-DTZ001 = Error(
-    message='DTZ001 The use of `datetime.datetime()` without `tzinfo` argument is not allowed.'
-)
+DTZ001 = Error(message="DTZ001 The use of `datetime.datetime()` without `tzinfo` argument is not allowed.")
 
 DTZ002 = Error(
-    message='DTZ002 The use of `datetime.datetime.today()` is not allowed. '
-    'Use `datetime.datetime.now(tz=)` instead.'
+    message="DTZ002 The use of `datetime.datetime.today()` is not allowed. Use `datetime.datetime.now(tz=)` instead."
 )
 
 DTZ003 = Error(
-    message='DTZ003 The use of `datetime.datetime.utcnow()` is not allowed. '
-    'Use `datetime.datetime.now(tz=)` instead.'
+    message="DTZ003 The use of `datetime.datetime.utcnow()` is not allowed. Use `datetime.datetime.now(tz=)` instead."
 )
 
 DTZ004 = Error(
-    message='DTZ004 The use of `datetime.datetime.utcfromtimestamp()` is not allowed. '
-    'Use `datetime.datetime.fromtimestamp(, tz=)` instead.'
+    message="DTZ004 The use of `datetime.datetime.utcfromtimestamp()` is not allowed. "
+    "Use `datetime.datetime.fromtimestamp(, tz=)` instead."
 )
 
-DTZ005 = Error(
-    message='DTZ005 The use of `datetime.datetime.now()` without `tz` argument is not allowed.'
-)
+DTZ005 = Error(message="DTZ005 The use of `datetime.datetime.now()` without `tz` argument is not allowed.")
 
-DTZ006 = Error(
-    message='DTZ006 The use of `datetime.datetime.fromtimestamp()` without `tz` argument is not allowed.'
-)
+DTZ006 = Error(message="DTZ006 The use of `datetime.datetime.fromtimestamp()` without `tz` argument is not allowed.")
 
 DTZ007 = Error(
-    message='DTZ007 The use of `datetime.datetime.strptime()` without %z must be followed by `.replace(tzinfo=)`.'
+    message="DTZ007 The use of `datetime.datetime.strptime()` without %z must be followed by `.replace(tzinfo=)`."
 )
 
 DTZ011 = Error(
-    message='DTZ011 The use of `datetime.date.today()` is not allowed. '
-    'Use `datetime.datetime.now(tz=).date()` instead.'
+    message="DTZ011 The use of `datetime.date.today()` is not allowed. Use `datetime.datetime.now(tz=).date()` instead."
 )
 
 DTZ012 = Error(
-    message='DTZ012 The use of `datetime.date.fromtimestamp()` is not allowed. '
-    'Use `datetime.datetime.fromtimestamp(, tz=).date()` instead.'
+    message="DTZ012 The use of `datetime.date.fromtimestamp()` is not allowed. "
+    "Use `datetime.datetime.fromtimestamp(, tz=).date()` instead."
 )
--- test_datetimez.py
+++ test_datetimez.py
@@ -8,10 +8,10 @@ class TestDateTimeZ(unittest.TestCase):
     def assert_codes(self, errors, codes):
         self.assertEqual(len(errors), len(codes))
         for error, code in zip(errors, codes):
-            self.assertTrue(error.message.startswith(code + ' '))
+            self.assertTrue(error.message.startswith(code + " "))
 
     def write_file_and_run_checker(self, content):
-        with tempfile.NamedTemporaryFile('w') as f:
+        with tempfile.NamedTemporaryFile("w") as f:
             f.write(content)
             f.flush()
             checker = DateTimeZChecker(None, f.name)
@@ -20,251 +20,177 @@ class TestDateTimeZ(unittest.TestCase):
     # DTZ001
 
     def test_DTZ001_args_good(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime(2000, 1, 1, 0, 0, 0, 0, datetime.timezone.utc)'
-        )
+        errors = self.write_file_and_run_checker("datetime.datetime(2000, 1, 1, 0, 0, 0, 0, datetime.timezone.utc)")
         self.assert_codes(errors, [])
 
     def test_DTZ001_kwargs_good(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime(2000, 1, 1, tzinfo=datetime.timezone.utc)'
-        )
+        errors = self.write_file_and_run_checker("datetime.datetime(2000, 1, 1, tzinfo=datetime.timezone.utc)")
         self.assert_codes(errors, [])
 
     def test_DTZ001_no_args(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime(2000, 1, 1, 0, 0, 0)'
-        )
-        self.assert_codes(errors, ['DTZ001'])
+        errors = self.write_file_and_run_checker("datetime.datetime(2000, 1, 1, 0, 0, 0)")
+        self.assert_codes(errors, ["DTZ001"])
 
     def test_DTZ001_no_args_unqualified(self):
-        errors = self.write_file_and_run_checker(
-            'datetime(2000, 1, 1, 0, 0, 0)'
-        )
-        self.assert_codes(errors, ['DTZ001'])
+        errors = self.write_file_and_run_checker("datetime(2000, 1, 1, 0, 0, 0)")
+        self.assert_codes(errors, ["DTZ001"])
 
     def test_DTZ001_none_args(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime(2000, 1, 1, 0, 0, 0, 0, None)'
-        )
-        self.assert_codes(errors, ['DTZ001'])
+        errors = self.write_file_and_run_checker("datetime.datetime(2000, 1, 1, 0, 0, 0, 0, None)")
+        self.assert_codes(errors, ["DTZ001"])
 
     def test_DTZ001_no_kwargs(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime(2000, 1, 1, fold=1)'
-        )
-        self.assert_codes(errors, ['DTZ001'])
+        errors = self.write_file_and_run_checker("datetime.datetime(2000, 1, 1, fold=1)")
+        self.assert_codes(errors, ["DTZ001"])
 
     def test_DTZ001_none_kwargs(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime(2000, 1, 1, tzinfo=None)'
-        )
-        self.assert_codes(errors, ['DTZ001'])
+        errors = self.write_file_and_run_checker("datetime.datetime(2000, 1, 1, tzinfo=None)")
+        self.assert_codes(errors, ["DTZ001"])
 
     # DTZ002
 
     def test_DTZ002(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.today()'
-        )
-        self.assert_codes(errors, ['DTZ002'])
+        errors = self.write_file_and_run_checker("datetime.datetime.today()")
+        self.assert_codes(errors, ["DTZ002"])
 
     def test_DTZ002_unqualified(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.today()'
-        )
-        self.assert_codes(errors, ['DTZ002'])
+        errors = self.write_file_and_run_checker("datetime.today()")
+        self.assert_codes(errors, ["DTZ002"])
 
     # DTZ003
 
     def test_DTZ003(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.utcnow()'
-        )
-        self.assert_codes(errors, ['DTZ003'])
+        errors = self.write_file_and_run_checker("datetime.datetime.utcnow()")
+        self.assert_codes(errors, ["DTZ003"])
 
     def test_DTZ003_unqualified(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.utcnow()'
-        )
-        self.assert_codes(errors, ['DTZ003'])
+        errors = self.write_file_and_run_checker("datetime.utcnow()")
+        self.assert_codes(errors, ["DTZ003"])
 
     # DTZ004
 
     def test_DTZ004(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.utcfromtimestamp(1234)'
-        )
-        self.assert_codes(errors, ['DTZ004'])
+        errors = self.write_file_and_run_checker("datetime.datetime.utcfromtimestamp(1234)")
+        self.assert_codes(errors, ["DTZ004"])
 
     def test_DTZ004_unqualified(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.utcfromtimestamp(1234)'
-        )
-        self.assert_codes(errors, ['DTZ004'])
+        errors = self.write_file_and_run_checker("datetime.utcfromtimestamp(1234)")
+        self.assert_codes(errors, ["DTZ004"])
 
     # DTZ005
 
     def test_DTZ005_args_good(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.now(datetime.timezone.utc)'
-        )
+        errors = self.write_file_and_run_checker("datetime.datetime.now(datetime.timezone.utc)")
         self.assert_codes(errors, [])
 
     def test_DTZ005_keywords_good(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.now(tz=datetime.timezone.utc)'
-        )
+        errors = self.write_file_and_run_checker("datetime.datetime.now(tz=datetime.timezone.utc)")
         self.assert_codes(errors, [])
 
     def test_DTZ005_no_args(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.now()'
-        )
-        self.assert_codes(errors, ['DTZ005'])
+        errors = self.write_file_and_run_checker("datetime.datetime.now()")
+        self.assert_codes(errors, ["DTZ005"])
 
     def test_DTZ005_no_args_unqualified(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.now()'
-        )
-        self.assert_codes(errors, ['DTZ005'])
+        errors = self.write_file_and_run_checker("datetime.now()")
+        self.assert_codes(errors, ["DTZ005"])
 
     def test_DTZ005_wrong_keywords(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.now(bad=datetime.timezone.utc)'
-        )
-        self.assert_codes(errors, ['DTZ005'])
+        errors = self.write_file_and_run_checker("datetime.datetime.now(bad=datetime.timezone.utc)")
+        self.assert_codes(errors, ["DTZ005"])
 
     def test_DTZ005_none_args(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.now(None)'
-        )
-        self.assert_codes(errors, ['DTZ005'])
+        errors = self.write_file_and_run_checker("datetime.datetime.now(None)")
+        self.assert_codes(errors, ["DTZ005"])
 
     def test_DTZ005_none_keywords(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.now(tz=None)'
-        )
-        self.assert_codes(errors, ['DTZ005'])
+        errors = self.write_file_and_run_checker("datetime.datetime.now(tz=None)")
+        self.assert_codes(errors, ["DTZ005"])
 
     # DTZ006
 
     def test_DTZ006_args_good(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.fromtimestamp(1234, datetime.timezone.utc)'
-        )
+        errors = self.write_file_and_run_checker("datetime.datetime.fromtimestamp(1234, datetime.timezone.utc)")
         self.assert_codes(errors, [])
 
     def test_DTZ006_keywords_good(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.fromtimestamp(1234, tz=datetime.timezone.utc)'
-        )
+        errors = self.write_file_and_run_checker("datetime.datetime.fromtimestamp(1234, tz=datetime.timezone.utc)")
         self.assert_codes(errors, [])
 
     def test_DTZ006_no_args(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.fromtimestamp(1234)'
-        )
-        self.assert_codes(errors, ['DTZ006'])
+        errors = self.write_file_and_run_checker("datetime.datetime.fromtimestamp(1234)")
+        self.assert_codes(errors, ["DTZ006"])
 
     def test_DTZ006_no_args_unqualified(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.fromtimestamp(1234)'
-        )
-        self.assert_codes(errors, ['DTZ006'])
+        errors = self.write_file_and_run_checker("datetime.fromtimestamp(1234)")
+        self.assert_codes(errors, ["DTZ006"])
 
     def test_DTZ006_wrong_keywords(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.fromtimestamp(1234, bad=datetime.timezone.utc)'
-        )
-        self.assert_codes(errors, ['DTZ006'])
+        errors = self.write_file_and_run_checker("datetime.datetime.fromtimestamp(1234, bad=datetime.timezone.utc)")
+        self.assert_codes(errors, ["DTZ006"])
 
     def test_DTZ006_none_args(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.fromtimestamp(1234, None)'
-        )
-        self.assert_codes(errors, ['DTZ006'])
+        errors = self.write_file_and_run_checker("datetime.datetime.fromtimestamp(1234, None)")
+        self.assert_codes(errors, ["DTZ006"])
 
     def test_DTZ006_none_keywords(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.fromtimestamp(1234, tz=None)'
-        )
-        self.assert_codes(errors, ['DTZ006'])
+        errors = self.write_file_and_run_checker("datetime.datetime.fromtimestamp(1234, tz=None)")
+        self.assert_codes(errors, ["DTZ006"])
 
     # DTZ007
 
     def test_DTZ007_good_replace(self):
         errors = self.write_file_and_run_checker(
-            'datetime.datetime.strptime(something, something).replace(tzinfo=datetime.timezone.utc)'
+            "datetime.datetime.strptime(something, something).replace(tzinfo=datetime.timezone.utc)"
         )
         self.assert_codes(errors, [])
 
     def test_DTZ007_good_astimezone(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.strptime(something, something).astimezone()'
-        )
+        errors = self.write_file_and_run_checker("datetime.datetime.strptime(something, something).astimezone()")
         self.assert_codes(errors, [])
 
     def test_DTZ007_good_format(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.strptime(something, "%H:%M:%S%z")'
-        )
+        errors = self.write_file_and_run_checker('datetime.datetime.strptime(something, "%H:%M:%S%z")')
         self.assert_codes(errors, [])
 
     def test_DTZ007_bad_format(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.strptime(something, "%H:%M:%S%Z")'
-        )
-        self.assert_codes(errors, ['DTZ007'])
+        errors = self.write_file_and_run_checker('datetime.datetime.strptime(something, "%H:%M:%S%Z")')
+        self.assert_codes(errors, ["DTZ007"])
 
     def test_DTZ007_no_replace_or_astimezone(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.strptime(something, something)'
-        )
-        self.assert_codes(errors, ['DTZ007'])
+        errors = self.write_file_and_run_checker("datetime.datetime.strptime(something, something)")
+        self.assert_codes(errors, ["DTZ007"])
 
     def test_DTZ007_no_replace_or_astimezone_unqualified(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.strptime(something, something)'
-        )
-        self.assert_codes(errors, ['DTZ007'])
+        errors = self.write_file_and_run_checker("datetime.strptime(something, something)")
+        self.assert_codes(errors, ["DTZ007"])
 
     def test_DTZ007_wrong_replace(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.datetime.strptime(something, something).replace(hour=1)'
-        )
-        self.assert_codes(errors, ['DTZ007'])
+        errors = self.write_file_and_run_checker("datetime.datetime.strptime(something, something).replace(hour=1)")
+        self.assert_codes(errors, ["DTZ007"])
 
     def test_DTZ007_none_replace(self):
         errors = self.write_file_and_run_checker(
-            'datetime.datetime.strptime(something, something).replace(tzinfo=None)'
+            "datetime.datetime.strptime(something, something).replace(tzinfo=None)"
         )
-        self.assert_codes(errors, ['DTZ007'])
+        self.assert_codes(errors, ["DTZ007"])
 
     # DTZ011
 
     def test_DTZ011(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.date.today()'
-        )
-        self.assert_codes(errors, ['DTZ011'])
+        errors = self.write_file_and_run_checker("datetime.date.today()")
+        self.assert_codes(errors, ["DTZ011"])
 
     def test_DTZ011_unqualified(self):
-        errors = self.write_file_and_run_checker(
-            'date.today()'
-        )
-        self.assert_codes(errors, ['DTZ011'])
+        errors = self.write_file_and_run_checker("date.today()")
+        self.assert_codes(errors, ["DTZ011"])
 
     # DTZ012
 
     def test_DTZ012(self):
-        errors = self.write_file_and_run_checker(
-            'datetime.date.fromtimestamp(1234)'
-        )
-        self.assert_codes(errors, ['DTZ012'])
+        errors = self.write_file_and_run_checker("datetime.date.fromtimestamp(1234)")
+        self.assert_codes(errors, ["DTZ012"])
 
     def test_DTZ012_unqualified(self):
-        errors = self.write_file_and_run_checker(
-            'date.fromtimestamp(1234)'
-        )
-        self.assert_codes(errors, ['DTZ012'])
+        errors = self.write_file_and_run_checker("date.fromtimestamp(1234)")
+        self.assert_codes(errors, ["DTZ012"])
